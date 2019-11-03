fe966d28d258f256b780a253f22673deddb05c84
Yuusuke <y38y38@gmail.com>
2019-11-03 23:58:40 +0900

    degbug

--- forYoutube/ConvYoutube.sh
+++ forYoutube/ConvYoutube.sh
@@ -1,1 +1,1 @@
-$1 -f rawvideo -pix_fmt yuv420p -s 1920x1080 -i $2 -r 29.97 -profile high -crf 20 ${2##.yuv}.mp4
+$1 -f rawvideo -pix_fmt yuv420p -s 1920x1080 -i $2 -vframes 300 -r 29.97 -profile high -crf 20 ${2##.yuv}.mp4
